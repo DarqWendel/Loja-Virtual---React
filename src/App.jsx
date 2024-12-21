2b447d148ee16da752e4a3d2541b0a69e454b4f5
unknown <darquiooliveira1007@digitalcollege.internal>
2024-12-21 16:53:34 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Painel from './pages/Painel';
+import Login from './pages/Login';
+import { useContext, useState} from 'react';
+import { AutenticacaoContext } from './context/AutenticacaoContext';
+
+function App() {
+  const {logado} = useContext(AutenticacaoContext)
+
+    if(logado) {
+      return <Painel/>
+    }
+    return <Login/>
+  }
+
+
+export default App
